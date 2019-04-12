bdc3bcd87f97918cced4479be19d99742ebc8641
bestils <thebestilspl@gmail.com>
2019-04-12 02:05:07 +0200

    finished tribonacii

--- src/main/scala/codewars/Tribonacci.scala
+++ src/main/scala/codewars/Tribonacci.scala
@@ -2,9 +2,9 @@ package codewars
 
 object Tribonacci extends App {
   def tribonacci[T: Numeric](signature: List[T], n: Int): List[T] = n match {
-
-    case 0 => signature
-    case _ => signature :+ signature.take(3).sum
+case   2|1|0  => signature.takeRight(n)
+    case 3 => signature
+    case _ => tribonacci(signature :+ signature.takeRight(3).sum,n-1)
 
   }
 
