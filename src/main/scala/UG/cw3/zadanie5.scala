8c770b00938f4d8b2fbffb6d66c0f314db2300b1
bestils <thebestilspl@gmail.com>
2019-04-14 10:32:57 +0200

    5 AND 6 FROM 3 ; 1 AND 2 FROM 4

--- src/main/scala/UG/cw3/zadanie5.scala
+++ src/main/scala/UG/cw3/zadanie5.scala
@@ -1,11 +1,18 @@
 package UG.cw3
 
-object zadanie5 {
+object zadanie5 extends App{
 //Zadanie 5. Stwórz rekurencyjną funckję ciag(n: Int): Int, która zwróci n-ty wyrażony wzorem:
   //F(0) = 1
   //F(1) = 1
   //F(n) = F(n-1) + F(n-2) dla n > 1
   //Rozwiąż to zadanie bez korzystania ze zmennych oraz wykorzystaj rekurencję ogonową.
   //Pierwsze 10 wyrazów ciągu: 1, 1, 2, 3, 5, 8, 13, 21, 34, 55.
-  
+
+
+  def giveFib(n : Int):Int= n match {
+    case 0 | 1 => n
+    case _ => giveFib( n-1 ) + giveFib( n-2 )
+
+  }
+
 }
--- src/main/scala/UG/cw4/zadanie2.scala
+++ src/main/scala/UG/cw4/zadanie2.scala
@@ -1,5 +1,10 @@
 package UG.cw4
 
-object zadanie2 {
+object zadanie2 extends App{
+  type Pred[A] = A => Boolean
+  def and[A](p: Pred[A], q: Pred[A]): Pred[A] = {n:A => p(n)&& p(q)}
+  def or[A](p: Pred[A], q: Pred[A]): Pred[A] = {n:A => p(n)|| p(q)}
+  def not[A](p: Pred[A]): Pred[A] = {n:A => !p(n)}
+  def imp[A](p: Pred[A], q: Pred[A]): Pred[A] = {n:A => (p(n)&& p(q))||(!p(n)&& p(q))}
 
 }
