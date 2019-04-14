3a62b41c24278d3e1835df825de5ce438454ec5e
bestils <thebestilspl@gmail.com>
2019-04-14 12:17:49 +0200

    little correction in 2/4

--- src/main/scala/UG/cw4/zadanie2.scala
+++ src/main/scala/UG/cw4/zadanie2.scala
@@ -2,13 +2,9 @@ package UG.cw4
 
 object zadanie2 extends App{
   type Pred[A] = A => Boolean
-
   def and[A](p: Pred[A], q: Pred[A]): Pred[A] = { a: A => p(a) && q(a) }
-
-  def or[A](p: Pred[A], q: Pred[A]): Pred[A] = { a: A => p(a) || q(a) }
-
-  def not[A](p: Pred[A]): Pred[A] = { a: A => !p(a) }
-
+  def or[A](p: Pred[A], q: Pred[A]): Pred[A] = {n:A => p(n)|| q(n)}
+  def not[A](p: Pred[A]): Pred[A] = {n:A => !p(n)}
   def imp[A](p: Pred[A], q: Pred[A]): Pred[A] = { a: A => !(p(a) && !q(a)) }
 
 }
