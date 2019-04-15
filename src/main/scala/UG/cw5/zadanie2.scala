37cb92236fe029fb1bcc256769f1785e40a09b85
Patryk_Laptop <Thebestilspl@gmail.com>
2019-04-15 13:31:49 +0200

    2 from  5

--- src/main/scala/UG/cw5/zadanie2.scala
+++ src/main/scala/UG/cw5/zadanie2.scala
@@ -7,5 +7,14 @@ object zadanie2 extends App{
   //Przykład:
   //Dla: seq = Seq(1, 2, 4, 6), a = 3 i leq = (_ < _), funckja powinna zwrócić: Seq(1, 2, 3, 4, 6).
 
-  def insertInto[A](a: A, seq: Seq[A])(leq: (A, A) => Boolean)
+  def insertInto[A](a: A, seq: Seq[A])(leq: (A, A) => Boolean) = {
+    def insertIntoHelper[A](a: A,seq : Seq[A])(leq : (A,A) => Boolean) (head : Seq[A]):Seq[A] = seq match {
+
+      case e+: s if leq(a,e) => (head :+ a :+ e) ++ s // ++ dodaje cały ciąg
+      case e+: s  => insertIntoHelper[A](a,s)(leq)(head:+e)
+      case Seq(e) if leq(a,e) => head  :+ a :+ e
+      case Seq(e) => head :+e :+ a
+  }
+insertIntoHelper(a,seq)(leq)(Seq())
+  }
 }
