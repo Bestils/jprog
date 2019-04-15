d150bfb3cb55ce50e1c257120975a9ab5dccafdd
Patryk_Laptop <Thebestilspl@gmail.com>
2019-04-15 11:24:57 +0200

    1 from 5

--- src/main/scala/UG/cw5/zadanie1.scala
+++ src/main/scala/UG/cw5/zadanie1.scala
@@ -5,4 +5,11 @@ object zadanie1 extends App{
   //def isOrdered[A](seq: Seq[A])(leq: (A, A) => Boolean): Boolean = /* ... */
   //która sprawdzi czy elementy sekwencji seq są ułożone zgodnie z porządkiem leq.
   // Zdefiniuj funkcję z użyciem rekurencji ogonowej.
+
+  def isOrdered[A](seq: Seq[A])(leq: (A, A) => Boolean): Boolean = seq match {
+    case Seq() => true
+    case Seq(_)  => true
+    case a +: b +: r if leq(a,b) =>isOrdered[A](b+:r)(leq)
+    case _ => false
+  }
 }
--- src/main/scala/UG/cw5/zadanie2.scala
+++ src/main/scala/UG/cw5/zadanie2.scala
@@ -6,4 +6,6 @@ object zadanie2 extends App{
   //która wstawi element a do sekwencji seq zgodnie z porządkiem określonym przez leq. Zdefiniuj funkcję z użyciem rekurencji ogonowej.
   //Przykład:
   //Dla: seq = Seq(1, 2, 4, 6), a = 3 i leq = (_ < _), funckja powinna zwrócić: Seq(1, 2, 3, 4, 6).
+
+  def insertInto[A](a: A, seq: Seq[A])(leq: (A, A) => Boolean)
 }
