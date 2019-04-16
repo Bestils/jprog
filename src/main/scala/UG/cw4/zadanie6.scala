4cfdd4b79ea6b834303687dac039a54ff7a6bffe
bestils <thebestilspl@gmail.com>
2019-04-16 16:16:49 +0200

    home to merge

--- src/main/scala/UG/cw4/zadanie6.scala
+++ src/main/scala/UG/cw4/zadanie6.scala
@@ -3,5 +3,12 @@ package UG.cw4
 object zadanie6 extends App{
 
 
-  def merge[A](a: Seq[A], b: Seq[A])(leq: (A, A) => Boolean): Seq[A]
+  def merge[A](a: Seq[A], b: Seq[A])(leq: (A, A) => Boolean): Seq[A]= { def mergeAux(a: Seq[A], b: Seq[A], mer: Seq[A]): Seq[A] = {
+    (a, b) match {
+      case _ if a.isEmpty || b.isEmpty => mer.reverse ++ a ++ b
+      case (e1 +: s1, e2 +: _) if leq(e1, e2) => mergeAux(s1, b, e1 +: mer)
+      case (_, e2 +: s2 ) => mergeAux(a, s2, e2 +: mer)
+
+  }
+
 }
