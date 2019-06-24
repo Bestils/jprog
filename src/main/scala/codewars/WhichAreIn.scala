6fe4d24a3d21be4c12890cd311431061e188cfec
bestils <thebestilspl@gmail.com>
2019-06-24 10:57:23 +0200

    i gave up with this hard exercise

---
+++ src/main/scala/codewars/WhichAreIn.scala
@@ -0,0 +1,23 @@
+//package codewars
+//
+//object WhichAreIn extends App {
+//  def inArray(array1: Array[String], array2: Array[String]): Array[String] = {
+//    def inArrayHelper(array1: Array[String], array2: Array[String], acc: Array[String] = Array()): Array[String] =  {
+//      if (array1.isEmpty){
+//        acc
+//      }
+//      else
+//        {
+//    if (array2.contains(array1.head)) { inArrayHelper(array1.tail, array2, acc.addString( array1.takeRight(1)) }
+//    else { inArrayHelper(array1.tail, array2, acc ) }
+//
+//
+//
+//    }
+//
+//    inArrayHelper(array1, array2)
+//
+//
+//  }
+//
+//}
