d536b7d2e54ae1b5a024e17a64b735e6836320aa
bestils <thebestilspl@gmail.com>
2019-07-03 13:42:53 +0200

    Exceptions

--- src/main/scala/myWork/Exceptions.scala
+++ src/main/scala/myWork/Exceptions.scala
@@ -1,7 +1,71 @@
 package myWork
 
+
 object Exceptions extends App {
 
-  val x:
+  val x: String = null
+
+
+
+
+// val fullArray : Array[Int] = Array.ofDim(Int.MaxValue )
+//  def soe:Int=soe +1
+//
+//  println(soe)
+}
+class OverflowException extends RuntimeException
+class UnderflowException extends RuntimeException
+class MatchCalculationException extends RuntimeException
+
+object PocketCalculator extends App{
+
+  def add(x:Int,y:Int) ={
+    val result = x+y
+  if(result>Int.MaxValue)
+    throw new OverflowException
+    else if(x>0&&y>0&&result<0)
+    throw new OverflowException
+  else if(x<0&&y<0&&result>0)
+    throw new UnderflowException
+else result
+
+  }
+  def substract(x:Int,y:Int) = {
+  val result =    x-y
+    if(result<Int.MinValue)
+      throw new UnderflowException
+    else if(x>0&&y>0&&result<0)
+      throw new OverflowException
+    else if(x<0&&y<0&&result>0)
+      throw new UnderflowException
+    else result
+  }
+  def multiply(x:Int,y:Int) = {
+    val result =    x-y
+    if(result<Int.MinValue)
+      throw new OverflowException
+    else if(x>0&&y>0&&result<0)
+      throw new OverflowException
+    else if(x<0&&y<0&&result>0)
+      throw new OverflowException
+    else if(x<0&&y>0&&result<0)
+      throw new OverflowException
+    else if(x<0&&y>0&&result>0)
+      throw new OverflowException
+    else result
+  }
+
+  def divide(x:Int,y:Int) ={
+    val result = x/y
+  if(y==0)
+    throw new MatchCalculationException
+  else result
+  }
+
+
+
+
+    println(add(Int.MaxValue,6))
+
 
 }
